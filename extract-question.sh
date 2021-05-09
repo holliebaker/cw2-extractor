59ce94291cb9a31dbda24a1d4939a2d93bef209a
Hollie Baker <hb581@linux1.bath.ac.uk>
2021-05-09 16:18:18 +0100

    fixing the script to actually work with the submissions
    - extracting files into a single dir
    - print better info when file not found

--- extract-question.sh
+++ extract-question.sh
@@ -17,21 +17,36 @@ mkdir -p $PROCESSED
 for f in $SUB_DIR/*.zip
 do
     # find the first folder in zip file called "Q$i"
-    DIR=`zipinfo -1 "$f" | grep -E "$QUESTION/$" | head -1`
+    DIR=`zipinfo -1 "$f" | grep -E "$QUESTION/" | head -1`
 
     if [ $(echo "$DIR" | wc -c) -le 1 ] # no idea why empty gives wc of 1??
     then
-        echo "WARNING: Unable to find \"$QUESTION\" in \"$f\""
-        continue
+        # some people have zipped all CWK2Qx.* in a single directory.
+        FILE=`zipinfo -1 "$f" | grep -E "CWK2$QUESTION" | head -1`
+
+        if [ $(echo "$FILE" | wc -c) -le 1 ] # no idea why empty gives wc of 1??
+        then
+            echo "WARNING: Unable to find \"$QUESTION\" in \"$f\""
+            zipinfo "$f"
+            echo ""
+
+            continue
+        fi
+        echo "NOTICE: \"$f\" could not find the directory, but found a source file."
+        echo ""
     fi
 
     # place in a new folder with unique name
     # extract the 8-digit participant number from moodle files
-    # ZIP_NAME=`echo $f | awk '{print substr($1, 13, 8)}'`
-    # for testing purposes - removes the .zip from end of name
-    ZIP_NAME=`echo "$f" | awk -F'/' '{print substr($NF, 1, length($NF)-4)}'`
+    ZIP_NAME=`echo "$f" | awk -F'/' '{print substr($NF, 13, 8)}'`
 
     OUT_DIR="$PROCESSED/$ZIP_NAME-$QUESTION"
-    unzip $f "$DIR" -d $OUT_DIR >> /dev/null
+    # NOTE: -o will overwrite duplicate files. this may not be what we want
+    if [[ -z $FILE ]]
+    then # standard directory structure
+        unzip -o -j "$f" "$DIR*" -d $OUT_DIR > /dev/null
+    else # single dir, multiple files
+        unzip -o -j "$f" "$FILE" -d $OUT_DIR > /dev/null
+    fi
 done
 
